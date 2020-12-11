9f5f33c8cb6f48bec51ca6c4e6155c142f10b96b
Duco Sebel <d.sebel@homewizard.nl>
2020-12-11 15:48:25 +0100

    Updated config flow user input

--- custom_components/homewizard_energy/config_flow.py
+++ custom_components/homewizard_energy/config_flow.py
@@ -1,9 +1,15 @@
 """Config flow for Homewizard Energy."""
-import aiohwenergy
+from aiohwenergy.hwenergy import SUPPORTED_DEVICES
 
 import logging
+
+from voluptuous.util import Lower
+
 from homeassistant import config_entries
 from homeassistant.helpers import config_entry_flow
+
+from voluptuous import Schema
+from voluptuous import Required, All, Length
 
 from .const import DOMAIN
 
@@ -28,7 +34,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle zeroconf discovery."""
 
         Logger.debug("config_flow async_step_zeroconf")
-        Logger.debug(discovery_info["properties"])
         
         entry_info = {
             "host": discovery_info["host"],
@@ -37,54 +42,56 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             "path": discovery_info["properties"]['path'] if 'path' in discovery_info["properties"] else None,
             "product_name": discovery_info["properties"]['product_name'] if 'product_name' in discovery_info["properties"] else None,
             "product_type": discovery_info["properties"]['product_type'] if 'product_type' in discovery_info["properties"] else None,
-            "unique_id": discovery_info["properties"]['serial'] if 'serial' in discovery_info["properties"] else None
+            "serial": discovery_info["properties"]['serial'] if 'serial' in discovery_info["properties"] else None
         }
-
-        Logger.debug(f"Found info: { entry_info['host'] } { entry_info['unique_id'] }.")
-
+        
         return await self.async_step_check(entry_info)
         
     async def async_step_check(self, entry_info):
         """Perform some checks and create entry if OK."""
         
         Logger.debug("config_flow async_step_check")
-
-        await self.async_set_unique_id(entry_info["unique_id"])
-        self._abort_if_unique_id_configured(updates=entry_info)
-
+        
+        if (entry_info["product_type"] not in SUPPORTED_DEVICES):
+            Logger.warning("Device not supported by integration")
+            return self.async_abort(reason="device_not_supported")
+        
         if entry_info["api_enabled"] != "1":
-            Logger.warning("API not enabled")
+            Logger.warning("API not enabled, please enable API in app")
             return self.async_abort(reason="api_not_enabled")
         
         Logger.debug(f"entry_info: {entry_info}")
         
-        if ((entry_info["host"] == None) or
-            (entry_info["port"] == None) or
-            (entry_info["unique_id"] == None) or
-            (entry_info["product_name"] == None) or
-            (entry_info["product_type"] == None) or
-            (entry_info["path"] == None) or
-            (entry_info["api_enabled"] == None)):
+        if (("host" not in entry_info) or
+            ("port" not in entry_info) or
+            ("serial" not in entry_info) or
+            ("product_name" not in entry_info) or
+            ("product_type" not in entry_info) or
+            ("path" not in entry_info) or
+            ("api_enabled" not in entry_info)):
             Logger.warning(f"Invalid discovery parameters")
             return self.async_abort(reason="invalid_discovery_parameters")
             
         self.context["host"] = entry_info["host"]
-        self.context["unique_id"] = entry_info["unique_id"]
+        self.context["unique_id"] = "%s_%s" % (entry_info['product_type'], entry_info['serial'])
+        self.context["serial"] = entry_info['serial']
         self.context["port"] = entry_info["port"]
-        self.context["product_name"] = entry_info["product_name"]
         self.context["path"] = entry_info["path"]
+        self.context["product_name"] = entry_info["product_name"]
         self.context["product_type"] = entry_info["product_type"]
         self.context["api_enabled"] = entry_info["api_enabled"]
+        
+        self.context["name"] = "%s (%s)" % (self.context["product_name"], self.context["serial"][-6:])
+        
+        await self.async_set_unique_id(self.context["unique_id"])
+        self._abort_if_unique_id_configured(updates=entry_info)
 
         self.context["title_placeholders"] = {
-            "name": self.context["product_name"],
+            "name": self.context["name"],
             "unique_id": self.context["unique_id"],
         }
-
-        # api = await async_get_aiohuesyncbox_from_entry_data(entry_info)
-        # if await api.is_registered():
-        #     await api.close()
-        # return await self._async_create_entry_from_context()
+        
+        # TODO Check if device is already configured (but maybe moved to new IP)
 
         return await self.async_step_discovery_confirm()
         
@@ -95,32 +102,26 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         
         errors = {}
         
-        if self.context["api_enabled"] != "1":
-            Logger.warning(f"API not enabled")
-            errors["base"] = "api_not_enabled"
-            return self.async_show_form(step_id="discovery_confirm", errors=errors)
+        schema = Schema({
+            Required('name'): All(str, Length(min=1, max=20))
+        })
         
         if user_input is None:
-            Logger.debug("async_step_discovery_confirm user_input is None")
             return self.async_show_form(
                 step_id="discovery_confirm",
-                description_placeholders={"name": "aaabbbccc"},
+                description_placeholders={"name": self.context["product_name"]},
+                data_schema=schema, 
+                errors=errors
             )
-        
-        Logger.debug("async_step_discovery_confirm _create_entry")
-        
-        return self.async_create_entry(
-            title=self.context["product_name"],
-            data=self.context,
-        )
-
-# async def _async_has_devices(hass) -> bool:
-#     """Return if there are devices that can be discovered."""
-#     # TODO Check if there are any devices that can be discovered in the network.
-#     devices = await hass.async_add_executor_job(aiohwenergy.discover)
-#     return len(devices) > 0
-
-
-# config_entry_flow.register_discovery_flow(
-#     DOMAIN, "Homewizard Energy", _async_has_devices, config_entries.CONN_CLASS_UNKNOWN
-# )
+        else:
+            Logger.debug("async_step_discovery_confirm _create_entry")
+            self.context["custom_name"] = user_input["name"] if user_input["name"] != "" else self.context["name"]
+            if (Lower(self.context["product_name"]) != Lower(user_input["name"])):
+                title = "%s (%s)" % (self.context["product_name"], self.context["custom_name"])
+            else:
+                title = self.context["custom_name"]
+                
+            return self.async_create_entry(
+                title=title,
+                data=self.context,
+            )        
--- custom_components/homewizard_energy/sensor.py
+++ custom_components/homewizard_energy/sensor.py
@@ -26,41 +26,59 @@ Logger = logging.getLogger(__name__)
 _PLATFORM = "sensor"
 
 SENSORS = {
-    const.ATTR_SMR_VERSION: {"icon": "mdi:pound", "unit": ""},
-    const.ATTR_METER_MODEL: {"icon": "mdi:counter", "unit": ""},
-    const.ATTR_WIFI_SSID: {"icon": "mdi:wifi", "unit": ""},
-    const.ATTR_WIFI_STRENGTH: {"icon": "mdi:wifi", "unit": PERCENTAGE},
+    const.ATTR_SMR_VERSION: {
+        "name": "SMR version", "icon": "mdi:pound", "unit": ""
+    },
+    const.ATTR_METER_MODEL: {
+        "name": "Model", "icon": "mdi:counter", "unit": ""
+    },
+    const.ATTR_WIFI_SSID: {
+        "name": "Wifi SSID", "icon": "mdi:wifi", "unit": ""
+    },
+    const.ATTR_WIFI_STRENGTH: {
+        "name": "Wifi Strength", "icon": "mdi:wifi", "unit": PERCENTAGE},
     const.ATTR_TOTAL_POWER_IMPORT_T1_KWH: {
+        "name": "Total power import T1",
         "icon": "mdi:home-import-outline",
         "unit": ENERGY_KILO_WATT_HOUR,
     },
     const.ATTR_TOTAL_POWER_IMPORT_T2_KWH: {
+        "name": "Total power import T2",
         "icon": "mdi:home-import-outline",
         "unit": ENERGY_KILO_WATT_HOUR,
     },
     const.ATTR_TOTAL_POWER_EXPORT_T1_KWH: {
+        "name": "Total power export T1",
         "icon": "mdi:home-export-outline",
         "unit": ENERGY_KILO_WATT_HOUR,
     },
     const.ATTR_TOTAL_POWER_EXPORT_T2_KWH: {
+        "name": "Total power export T2",
         "icon": "mdi:home-export-outline",
         "unit": ENERGY_KILO_WATT_HOUR,
     },
-    const.ATTR_ACTIVE_POWER_W: {"icon": "mdi:transmission-tower", "unit": POWER_WATT},
+    const.ATTR_ACTIVE_POWER_W: {
+        "name": "Active power", "icon": "mdi:transmission-tower", "unit": POWER_WATT
+    },
     const.ATTR_ACTIVE_POWER_L1_W: {
+        "name": "Active power L1",
         "icon": "mdi:transmission-tower",
         "unit": POWER_WATT,
     },
     const.ATTR_ACTIVE_POWER_L2_W: {
+        "name": "Active power L2",
         "icon": "mdi:transmission-tower",
         "unit": POWER_WATT,
     },
     const.ATTR_ACTIVE_POWER_L3_W: {
+        "name": "Active power L3",
         "icon": "mdi:transmission-tower",
         "unit": POWER_WATT,
     },
-    const.ATTR_TOTAL_GAS_M3: {"icon": "mdi:fire", "unit": VOLUME_CUBIC_METERS},
-    const.ATTR_GAS_TIMESTAMP: {"icon": "mdi:timeline-clock", "unit": ""},
+    const.ATTR_TOTAL_GAS_M3: {
+        "name": "Total gas", "icon": "mdi:fire", "unit": VOLUME_CUBIC_METERS},
+    const.ATTR_GAS_TIMESTAMP: {
+        "name": "Gas timestamp", "icon": "mdi:timeline-clock", "unit": ""},
 }
 
 
@@ -69,7 +87,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     Logger.info("Setting up sensor for HomeWizard Energy.")
 
     energy_api = aiohwenergy.HomeWizardEnergy(entry.data["host"])
-    
+
     await energy_api.initialize()
 
     async def async_update_data():
@@ -81,42 +99,42 @@ async def async_setup_entry(hass, entry, async_add_entities):
         async with async_timeout.timeout(10):
             try:
                 await energy_api.data.update()
-                
+
                 data = {}
                 for datapoint in energy_api.data.available_datapoints:
                     data[datapoint] = getattr(energy_api.data, datapoint)
-                
+
                 return data
             except AttributeError:
                 return
-    
+
     # Determine update interval
-    ## Default update interval
+    # Default update interval
     update_interval = 5
-    
+
     try:
         product_type = energy_api.device.product_type
     except AttributeError:
         product_type = "Unknown"
-        
+
     if (product_type == "HWE-P1"):
         try:
             smr_version = energy_api.data.smr_version
             if (smr_version == 50):
-                update_interval = 5 # TODO set back to 1, Throttle down for now, 
+                update_interval = 5  # TODO set back to 1, Throttle down for now,
             else:
                 update_interval = 5
         except AttributeError:
             pass
-        
+
     if (product_type == "SDM230-wifi" or product_type == "SDM630-wifi"):
         update_interval = 1
-    
+
     coordinator = DataUpdateCoordinator(
         hass,
         Logger,
         # Name of the data. For logging purposes.
-        name=entry.data["product_name"],
+        name=entry.data["name"],
         update_method=async_update_data,
         # Polling interval. Will only be polled if there are subscribers.
         update_interval=timedelta(seconds=update_interval),
@@ -133,9 +151,10 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
     return True
 
+
 class device_hwe_p1(CoordinatorEntity):
     """Representation of a HomeWizard Energy"""
-    
+
     host = None
     name = None
     unique_id = None
@@ -147,15 +166,15 @@ class device_hwe_p1(CoordinatorEntity):
         super().__init__(coordinator)
 
         # Config attributes.
-        self.name = f"{ entry_data['product_name'] } {info_type}"
-        
+        self.name = "%s %s" % (entry_data['custom_name'], SENSORS[info_type]["name"])
+
         self.api = None
 
         self.host = entry_data["host"]
         self.info_type = info_type
         self.coordinator = coordinator
         self.entry_data = entry_data
-        self.unique_id = f"{ entry_data['product_type'] }_{entry_data['unique_id']}_{info_type}"
+        self.unique_id = "%s_%s" % (entry_data['unique_id'], info_type)
 
     @property
     def icon(self):
@@ -172,6 +191,7 @@ class device_hwe_p1(CoordinatorEntity):
         """Return the unit of measurement."""
         return SENSORS[self.info_type]["unit"]
 
+
 async def async_get_aiohwenergy_from_entry_data(entry_data):
     """Create a HomewizardEnergy object from entry data."""
 
@@ -180,6 +200,6 @@ async def async_get_aiohwenergy_from_entry_data(entry_data):
         % (__name__, str(entry_data))
     )
 
-    return aiohwenergy.HomeWizardEnergy (
+    return aiohwenergy.HomeWizardEnergy(
         entry_data["host"]
     )
